d383ba5fad52384f9be3109c012a2681f7ebff78
harsh538 <harshadvele538@gmail.com>
2022-05-03 16:23:27 +0530

    Header

--- src/app/button-click/button-click.component.ts
+++ src/app/button-click/button-click.component.ts
@@ -6,12 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-click.component.css']
 })
 export class ButtonClickComponent {
-public title='Nested Loops';
-
-users=[
-  {name:'Harshad',city:'Mumbai',socialAc:['facebook','linkedin','gmail']},
-  {name:'Vishal',city:'Chennai',socialAc:['youtube','','gmail','java']},
-  {name:'Subodh',city:'Nanded',socialAc:['facebook','linkedin','gmail']},
-  {name:'Sudhir',city:'Kalyan',socialAc:['facebook','linkedin','gmail']},
-]
+public title='Header';
 }
