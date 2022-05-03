853fb77ddef71743a683becef1999bef73de0739
harsh538 <harshadvele538@gmail.com>
2022-05-03 21:48:03 +0530

    To do list program using angular

--- src/app/button-click/button-click.component.ts
+++ src/app/button-click/button-click.component.ts
@@ -6,5 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-click.component.css']
 })
 export class ButtonClickComponent {
-public title='Alert in Bootstrap';
+  public title = 'To do List in Angular';
+  list: any[] = [];
+  addTask(t: any) {
+    this.list.push({ id: this.list.length, name: t });
+    console.warn(t);
+  }
+  removeTask(id: number) {
+    console.warn(id);
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
