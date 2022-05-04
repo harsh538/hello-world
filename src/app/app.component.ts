06de9694351f5ae6af77dfadcf3439b26d75c2ac
harsh538 <harshadvele538@gmail.com>
2022-05-04 18:01:36 +0530

    Basics of Type Script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Template Reference Variable';
-  callFunction(val:any)
-  {
-    console.warn(val);
+  title = 'Basics of Type Script';
+  data=20;
+  object:{name:string;pho:number}={name:'Harshad',pho:32423}; // Object in TS
+  arrayy:string[]=['apple','mango','pineapple','orenge'];     //array in TS
+  //pipeline character used for assiging mulitiple datatypes.
+  getDetails(item:string| boolean){
+    console.warn(item);
+  }
+  update(){
+    this.getDetails(true);
   }
 }
