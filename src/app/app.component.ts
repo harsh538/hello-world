a1e6e8ac917b9e8aacd232b951e8ca5adc294b53
harsh538 <harshadvele538@gmail.com>
2022-05-04 12:25:15 +0530

    Pass data child to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Code Step By Step';
+  data=10;
+  updateChild(){
+    this.data=Math.floor(Math.random()*10);
+  }
 }
--- src/app/button-click/button-click.component.ts
+++ src/app/button-click/button-click.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-click',
   templateUrl: './button-click.component.html',
   styleUrls: ['./button-click.component.css']
 })
+
 export class ButtonClickComponent {
-  public title = 'To do List in Angular';
-  list: any[] = [];
-  addTask(t: any) {
-    this.list.push({ id: this.list.length, name: t });
-    console.warn(t);
-  }
-  removeTask(id: number) {
-    console.warn(id);
-    this.list = this.list.filter(item => item.id !== id);
-  }
+  public title = 'pass data child to parent component';
+  @ Input() item=0;
 }
