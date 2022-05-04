c92a39d8e62d0f6f755dd1b33a3a51b3769f22b8
harsh538 <harshadvele538@gmail.com>
2022-05-04 18:17:28 +0530

    Basic Pipes in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Basics of Type Script';
-  data=20;
-  object:{name:string;pho:number}={name:'Harshad',pho:32423}; // Object in TS
-  arrayy:string[]=['apple','mango','pineapple','orenge'];     //array in TS
-  //pipeline character used for assiging mulitiple datatypes.
-  getDetails(item:string| boolean){
-    console.warn(item);
-  }
-  update(){
-    this.getDetails(true);
-  }
+  title = 'Pipes in Angular';
+  text="Hello World";
+  date= Date();
 }
